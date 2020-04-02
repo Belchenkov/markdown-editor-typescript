7a7077100cb6c9685e79cc5cc3f764c07b45fb21
Aleksey Belchenkov <u608110@gmail.com>
2020-04-02 19:52:19 +0300

    Create HTML Structure Project

---
+++ script/MarkdownParser.ts
@@ -0,0 +1,7 @@
+class HtmlHandler {
+    public TextChangeHandler(id: string, output: string) : void {
+        let markdown = <HTMLTextAreaElement>document.getElementById(id);
+        let markdownOutput = <HTMLLabelElement>document.getElementById(output);
+
+    }
+}
