fa0c2070d8e424232f8989e1e116602010a5de3e
Aleksey Belchenkov <u608110@gmail.com>
2020-04-03 19:03:19 +0300

    Building our Bootstrap UI

--- script/MarkdownParser.ts
+++ script/MarkdownParser.ts
@@ -3,5 +3,14 @@ class HtmlHandler {
         let markdown = <HTMLTextAreaElement>document.getElementById(id);
         let markdownOutput = <HTMLLabelElement>document.getElementById(output);
 
+        if (markdown !== null) {
+            markdown.onkeyup = e => {
+                if (markdown.value) {
+                    markdownOutput.innerHTML = markdown.value;
+                } else {
+                    markdownOutput.innerHTML = "<p></p>";
+                }
+            }
+        }
     }
 }
