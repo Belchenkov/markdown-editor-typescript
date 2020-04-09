9d66bcc45bf82eb721a6d1304def39782c134fe9
Aleksey Belchenkov <u608110@gmail.com>
2020-04-09 19:35:11 +0300

    Class ParseChainHandler

--- MarkdownParser.ts
+++ MarkdownParser.ts
@@ -39,6 +39,25 @@ abstract class VisitorBase implements IVisitor {
 
 }
 
+abstract class Handler<T> {
+    protected next : Handler<T> | null = null;
+
+    public SetNext(next : Handler<T>) : void {
+        this.next = next;
+    }
+
+    public HandleRequest(request : T) : void {
+        if (!this.CanHandle(request)) {
+            if (this.next !== null) {
+                this.next.HandleRequest(request);
+            }
+            return;
+        }
+    }
+
+    protected abstract CanHandle(request : T) : boolean;
+}
+
 class MarkdownDocument implements IMarkdownDocument {
     private content : string = "";
 
@@ -103,8 +122,6 @@ class TagTypeToHtml {
     public ClosingTag(tagType : TagType) : string {
         return this.GetTag(tagType, `</`);
     }
-
-
 }
 
 class Visible implements IVisitable {
@@ -140,5 +157,48 @@ class ParagraphVisitor extends VisitorBase {
 class HorizontalRuleVisitor extends VisitorBase {
     constructor() {
         super(TagType.HorizontalRule, new TagTypeToHtml());
+    }
+}
+
+class ParseChainHandler extends Handler<ParseElement> {
+    private readonly visitable : IVisitable = new Visible();
+
+    constructor(
+        private readonly document : IMarkdownDocument,
+        private readonly tagType : string,
+        private readonly visitor : IVisitor
+        ) {
+        super();
+    }
+
+    protected CanHandle(request: ParseElement): boolean {
+        let split = new LineParser().Parse(request.CurrentLine, this.tagType);
+
+        if (split[0]) {
+            request.CurrentLine = split[1];
+            this.visitable.Accept(this.visitor, request, this.document);
+        }
+
+        return split[0];
+    }
+}
+
+class LineParser {
+    public Parse(value : string, tag : string) : [boolean, string] {
+        let output : [boolean, string] = [false, ""];
+        output[1] = value;
+
+        if (value === "") {
+            return output;
+        }
+
+        let split = value.startsWith(`${tag}`);
+
+        if (split) {
+            output[0] = true;
+            output[1] = value.substr(tag.length);
+        }
+
+        return output;
     }
 }
